a1d30be79302cd70c43843437e1a3902afe27bdf
rain <rain@localhost.localdomain>
2017-10-09 11:42:14 +0800

    lfs替换掉shell命令ls

--- app/routers.lua
+++ app/routers.lua
@@ -1,4 +1,5 @@
 local log = require "utils.log"
+local lfs = require "lfs"
 
 -- 将路由和function映射关系加进去
 local routers = {
@@ -17,20 +18,26 @@ function _M.set_routers()
     routers['OPTIONS'] = {}
     routers['HEAD'] = {}
 
-    filenames = io.popen("ls " .. os.getenv('app_root') .. "/app/controllers/*.lua")
-    for filename in filenames:lines() do
-        log.debug(filename)
-        begin = #filename - filename:reverse():find("/") + 2
-        lua_filename = string.sub(filename, begin, -5)
-        log.crit(lua_filename)
-        local module = require("controllers." .. lua_filename)
+    -- filenames = io.popen("ls " .. os.getenv('app_root') .. "/app/controllers/*.lua")
+    -- for filename in filenames:lines() do
+    local path = os.getenv('app_root') .. "/app/controllers/"
+    for filename in lfs.dir(path) do
+        log.crit(filename)
+        -- begin = #filename - filename:reverse():find("/") + 2
+        local lua_filename = string.sub(filename, 1, -5)
+        local suffix = string.sub(filename, -4)
+        log.debug(lua_filename, " ", suffix)
+        local attr = lfs.attributes(path .. filename)
+        if suffix == '.lua' and attr.mode == 'file' then
+            local module = require("controllers." .. lua_filename)
 
-        if module.router then
-            for k, v in pairs(module:router()) do
-                log.crit(k)
-                for key, val in pairs(v) do
-                    log.crit(key)
-                    routers[k][key] = val
+            if module.router then
+                for k, v in pairs(module:router()) do
+                    log.crit(k)
+                    for key, val in pairs(v) do
+                        log.crit(key)
+                        routers[k][key] = val
+                    end
                 end
             end
         end
